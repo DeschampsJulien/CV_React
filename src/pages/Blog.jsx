5bccbd9790d8fb59fd10d591435dd4d330fe3722
Julien Deschamps <jdeschamps34@yahoo.fr>
2024-01-21 09:57:12 +0100

    CV React 1.0

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -22,7 +22,7 @@ export default function Blog() {
 							<h3 class="card-title"><strong>Coder sont site en HTML et CSS</strong></h3>
 							<p class="card-text">Tous les jours, tu surfes sur le net, mais sais-tu comment tout cela fonctionne ? Si tu as déjà vu du code informatique, tu t’ai sûrement dit que ça semblait bien compliqué ! Et pourtant,</p>
 						</div>
-						<a class="btn btn-primary" href="https://www.webcup.fr/boutique/je-cree-mon-premier-site-web-avec-html-css-ou-wordpress" role="button">Lire la suite</a>
+						<a class="btn btn-primary" href="https://www.webcup.fr/boutique/je-cree-mon-premier-site-web-avec-html-css-ou-wordpress" target="_blank" role="button">Lire la suite</a>
 						<div class="card-footer">
 							<small class="text-muted">Dernière mise à jour il y a 3 minutes</small>
 						</div>
@@ -35,7 +35,7 @@ export default function Blog() {
                             <h3 class="card-title"><strong>Vendre un produit sur le WEB</strong></h3>
                             <p class="card-text">Avec l’explosion d’internet et des réseaux sociaux, il n’a jamais été aussi simple de vendre ses produits. Du moins, c’est l’impression que nous avons en parcourant les milliers de pages de vente de produits de tous les genres.</p>
                         </div>
-						<a class="btn btn-primary" href="https://lasagadesaudacieux.com/vente-de-produits/" role="button">Lire la suite</a>
+						<a class="btn btn-primary" href="https://lasagadesaudacieux.com/vente-de-produits" target="_blank" role="button">Lire la suite</a>
 					
                         
                         <div class="card-footer">
@@ -48,9 +48,9 @@ export default function Blog() {
                         <img src={Position} alt="Se positionner sur Google" className="rounded-top"/>
                         <div class="card-body">
                             <h3 class="card-title"><strong>Se positionner sur Google</strong></h3>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <p class="card-text">Être en première position sur Google reste la meilleure façon pour attirer plus de visiteurs sur votre site web, et donc, d’attirer de nouveaux clients.</p>
                         </div>
-                        {/* https://falia.co/ressources/comment-etre-en-premiere-position-google/ */}
+						<a class="btn btn-primary" href="https://falia.co/ressources/comment-etre-en-premiere-position-google" target="_blank" role="button">Lire la suite</a>
                         <div class="card-footer">
                             <small class="text-muted">Dernière mise à jour il y a 3 minutes</small>
                         </div>
@@ -61,9 +61,9 @@ export default function Blog() {
 						<img src={Responsive} alt="Coder en responsive design" className="rounded-top"/>
 						<div class="card-body">
 							<h3 class="card-title"><strong>Coder en responsive design</strong></h3>
-							<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+							<p class="card-text">Contrairement aux brochures et autres supports marketing qui sont réalisés au moment de leur impression, un site Web n'est jamais « terminé ». Lancer un site Web n’est que la première étape de sa vie.</p>
 						</div>
-                        {/* https://negliadesign.com/web/top-three-benefits-of-responsive-website-design/ */}
+						<a class="btn btn-primary" href="https://negliadesign.com/web/top-three-benefits-of-responsive-website-design" target="_blank" role="button">Lire la suite</a>
 						<div class="card-footer">
 							<small class="text-muted">Dernière mise à jour il y a 3 minutes</small>
 						</div>
@@ -74,9 +74,9 @@ export default function Blog() {
 					    <img src={Seo} alt="Techniques de référencement" className="rounded-top"/>
                         <div class="card-body">
                             <h3 class="card-title"><strong>Techniques de référencement</strong></h3>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            <p class="card-text">Le SEO (Search Engine Optimization), ou référencement naturel, consiste à optimiser les éléments d’un site internet pour améliorer son positionnement dans les moteurs de recherche.</p>
                         </div>
-                        {/* https://www.noiise.com/guide/seo/ */}
+						<a class="btn btn-primary" href="https://www.noiise.com/guide/seo" target="_blank" role="button">Lire la suite</a>
                         <div class="card-footer">
                             <small class="text-muted">Dernière mise à jour il y a 3 minutes</small>
                         </div>
@@ -87,9 +87,9 @@ export default function Blog() {
                         <img src={Apprendre} alt="Apprendre à coder" className="rounded-top"/>
                         <div class="card-body">
                             <h3 class="card-title"><strong>Apprendre à coder</strong></h3>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            <p class="card-text">Le CPF permet à toute personne, salariée ou demandeur d’emploi, de suivre la formation de leur choix.</p>
                         </div>
-                        {/* https://www.centre-europeen-formation.fr/ */}
+						<a class="btn btn-primary" href="https://www.centre-europeen-formation.fr" target="_blank" role="button">Lire la suite</a>
                         <div class="card-footer">
                             <small class="text-muted">Dernière mise à jour il y a 3 minutes</small>
                         </div>
