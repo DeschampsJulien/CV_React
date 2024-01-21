f11cbc311f4483f4daebed12529ce7e83ef9e4c1
Julien Deschamps <jdeschamps34@yahoo.fr>
2024-01-21 15:40:23 +0100

    CV React 1.1

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -18,31 +18,36 @@ export default function Accueil() {
                 <div className="container">
                     <h2 className="text-uppercase">À propos</h2>
                     <hr/>
+
                     <div className="row">
                         <article className="col-md-6">
-                            <h3>Mon histoire</h3>
-                            <p className="fs-5">
-                            A la recherche, depuis quelques temps, d'une nouvelle activité professionnelle, les métiers du Web ont suscité ma curiosité.
-                            Ainsi, poussé par cette envie d'en savoir plus sur cette technologie si présente dans notre quotidien, me voilà parti pour trouver des réponses.  
-                            De nombreux langages informatiques, jargon technique et outils de développement entourent ce métier, il n'est pas si facile de s'y retrouver !  
-                            Une découverte dans un cadre professionnel me semble essentiel avant toute chose.
-                            La chance, une opportunité, un codeur passionné par le développement Web m'a initié au premier langage, semaine après semaine je me suis pris aux
-                            jeux de la création! Pour aujourd'hui vous présenter mon Portfolio.     
-                            </p>
+                            <div className="p-4 m-2 bg-light text-center text-break border rounded">
+                                <h3>Mon histoire</h3>
+                                <p className="fs-5">
+                                A la recherche, depuis quelques temps, d'une nouvelle activité professionnelle, les métiers du Web ont suscité ma curiosité.
+                                Ainsi, poussé par cette envie d'en savoir plus sur cette technologie si présente dans notre quotidien, me voilà parti pour trouver des réponses.  
+                                De nombreux langages informatiques, jargon technique et outils de développement entourent ce métier, il n'est pas si facile de s'y retrouver !  
+                                Une découverte dans un cadre professionnel me semble essentiel avant toute chose.
+                                La chance, une opportunité, un codeur passionné par le développement Web m'a initié au premier langage, semaine après semaine je me suis pris aux
+                                jeux de la création! Pour aujourd'hui vous présenter mon Portfolio.     
+                                </p>
+                            </div>  
                         </article>
 
                         {/* SUPRESSION DE LA BALISE "HR" DE MD A XXL */}
-                        <hr className="d-md-none d-lg-none d-xl-none d-xxl-none"/>
+                        {/* <hr className="d-md-none d-lg-none d-xl-none d-xxl-none"/> */}
                         
                         <article className="col-md-6">
-                            <h3>Mes valeurs</h3>
-                            <p className="fs-5">	
-                            Identifier, prioriser et trouver des solutions afin de répondre aux attentes,  
-                            accroître le niveau de satisfaction et pérenniser, telles sont les valeurs 
-                            primordiales auxquelles je m'attache. De plus, une politique managériale basée sur
-                            le développement des compétences et l'implication pour le travail en équipe,
-                            est pour moi un objectif et une nécessité au seins d'un groupe.
-                            </p>
+                        <div className="p-4 m-2 bg-light text-center text-break border rounded">
+                                <h3>Mes valeurs</h3>
+                                <p className="fs-5">	
+                                Identifier, prioriser et trouver des solutions afin de répondre aux attentes,  
+                                accroître le niveau de satisfaction et pérenniser, telles sont les valeurs 
+                                primordiales auxquelles je m'attache. De plus, une politique managériale basée sur
+                                le développement des compétences et l'implication pour le travail en équipe,
+                                est pour moi un objectif et une nécessité au seins d'un groupe.
+                                </p>
+                            </div> 
                         </article>
                     </div>
                     <a className="btn btn-xl btn-outline-light" href="../">
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,73 @@
-export default function Accueil() {
+export default function Contact() {
     return (
-        <h1>Contact</h1>
+
+        <main className="text-center py-5 px-4 mt-4">
+
+			<h2 className="text-uppercase mt-5"><strong>Contact</strong></h2>
+			<p className="text-uppercase mb-5">Pour me contacter en vue d'un entretien ou d'une future collaboration, merci de remplir le formulaire de contact.</p>
+			<hr />
+
+            <div className="container">
+                <div className="row">
+
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleInputEmail1">Email address</label>
+                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                            <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Password</label>
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" class="form-check-input" id="exampleCheck1"/>
+                            <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+
+                    <form>
+                        <div class="form-group">
+                            <label for="exampleFormControlInput1">Email address</label>
+                            <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect1">Example select</label>
+                            <select class="form-control" id="exampleFormControlSelect1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlSelect2">Example multiple select</label>
+                            <select multiple class="form-control" id="exampleFormControlSelect2">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleFormControlTextarea1">Example textarea</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </form>
+
+
+
+
+                </div>
+            </div>
+            
+        
+            
+        </main>
+        
 
     );
 }
